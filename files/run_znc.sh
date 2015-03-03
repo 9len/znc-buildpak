461d0ada6119eba855591de2d1861c4f1de9ecfd
Glen D Sanford <9len@users.noreply.github.com>
2015-03-03 13:55:50 -0800

    ok that doesn't work

--- files/run_znc.sh
+++ files/run_znc.sh
@@ -1,9 +1,6 @@
 ./znc/bin/znc -f &
 export ZNCPID=$!
-if [ -n "$NGROK_SUBDOMAIN" ]; then
-  SUBDOMAIN_FLAG="-subdomain $NGROK_SUBDOMAIN"
-fi
-./ngrok -authtoken $NGROK_API_KEY -log=stdout $SUBDOMAIN_FLAG --config ngrok.conf start znc &
+./ngrok -authtoken $NGROK_API_KEY -log=stdout --config ngrok.conf start znc &
 export NGROKPID=$!
 echo "waiting for znc ($ZNCPID) to exit......."
 while [ -e /proc/$ZNCPID ]

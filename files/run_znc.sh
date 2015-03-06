2ec801e3f8729d64db9ad2f8752d52857b2cf8e1
Glen D Sanford <9len@users.noreply.github.com>
2015-03-06 10:59:58 -0800

    spacing

--- files/run_znc.sh
+++ files/run_znc.sh
@@ -1,8 +1,11 @@
 ./znc/bin/znc -f &
 export ZNCPID=$!
+
 ./ngrok -authtoken $NGROK_API_KEY -log=stdout -config ngrok.conf start znc &
 export NGROKPID=$!
+
 echo "waiting for znc ($ZNCPID) or ngrok ($NGROKPID) to exit......."
+
 while [ -e /proc/$ZNCPID -a -e /proc/$NGROKPID]
 do
     echo "znc ($ZNCPID) is running"
